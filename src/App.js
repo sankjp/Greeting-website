c1f15b28a821385947995e4baa4006b856676ff4
santanu banerjee <santanub65@gmail.com>
2021-11-11 16:43:42 +0530

    greeting website using inline css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+  let currentData=new Date();
+  currentData= currentData.getHours();
+  let greeting='';
+  const style={};
+
+  if (currentData >=3 && currentData<12) {
+    greeting="Good Morning";
+    style.color='green';
+  } else if (currentData >=12 && currentData <17){
+    greeting="Good Afternoon";
+    style.color='blue';
+  } else if (currentData >=17 && currentData <19 ) {
+    greeting="Good Evening";
+    style.color='orange';
+  } else {
+    greeting="Good Night";
+    style.color='black';
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hellow sir or madam <span style={style}><u>{greeting}</u></span></h1>
     </div>
   );
 }
